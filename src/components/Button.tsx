84e0518eaf3803378051ef8527be191d4248a30a
WilliamFilh0 <williamfilho8908@gmail.com>
2024-05-07 11:00:29 -0300

    feat: Handling multiple routes

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainner, ButtonVariant } from "./Button.styles";
-
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-  return <ButtonContainner variant={variant}>Enviar</ButtonContainner>;
-}
