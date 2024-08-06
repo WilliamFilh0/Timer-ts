ba8b485e9ee108adabbcbda7b133bd6263e72819
WilliamFilh0 <williamfilho8908@gmail.com>
2024-08-06 09:51:28 -0300

    feat: Improving the Inputs.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,14 +17,24 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em </label>
           <TaskInput
             id="task"
+            list="task-suggestions"
             placeholder="De um nome pro seu projeto"
           ></TaskInput>
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
