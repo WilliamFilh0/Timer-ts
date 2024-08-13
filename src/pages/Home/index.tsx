669b6e1d21ae3bd73b1b412056d16058b5cd93e6
WilliamFilh0 <williamfilho8908@gmail.com>
2024-08-13 10:17:49 -0300

    feat: creating a countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -39,6 +39,7 @@ interface Cycle {
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCicleFormValidationSchema),
@@ -67,6 +68,17 @@ export function Home() {
 
   // Mostar na tela o ciclo Ativo.
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  //Fazendo com que tenha sempre dois caracteres, quando tiver minutos abaixo de 10.
+  const minutes = String(minutesAmount).padStart(2, "0");
+
+  const seconds = String(secondsAmount).padStart(2, "0");
 
   console.log(activeCycle);
 
@@ -106,11 +118,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
